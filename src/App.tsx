468b9c69b7aff8d56e02744ef9b0c3d7ae3e4ab7
Froggher <federicoranocchia@gmail.com>
2023-10-19 16:50:54 +0200

    ♻️ Spostato in app.tsx il react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import NavBar from "./misc/NavBar";
+import ErrorPage from "./ErrorPage/ErrorPage";
+import SearchMap from "./SearchMap/SearchMap";
+import TestDatabase from "./TestDatabase/TestDatabase";
+import SignUp from "./User/SignUp";
+import About from "./About/About";
+import Login from "./User/Login";
 
 
 function App() {
 
 
+/* Inizializzazione react router */
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NavBar />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "map",
+        element: <SearchMap />,
+      },
+      {
+        path: "testdatabase",
+        element: <TestDatabase />,
+      },
+      {
+        path: "user",
+        element: <Login />,
+      },
+      {
+        path: "signup",
+        element: <SignUp />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 
   return (
     <>
+      <RouterProvider router={router} />
       <h2>Questa é la homepage</h2>
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,58 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import App from './App.tsx'
-import ErrorPage from './ErrorPage/ErrorPage.tsx';
-import NavBar from './misc/NavBar.tsx';
-import About from './About/About.tsx';
-import TestDatabase from './TestDatabase/TestDatabase.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import SearchMap from './SearchMap/SearchMap.tsx';
-import Login from './User/Login.tsx';
-import SignUp from './User/SignUp.tsx';
+import App from './App';
 //import './index.css'
 
 
 /* Inizializzazione react query */
 const queryClient = new QueryClient()
-
-/* Inizializzazione react router */
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <NavBar />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "app",
-        element: <App />,
-      },
-      {
-        path: "map",
-        element: <SearchMap />,
-      },
-      {
-        path: "testdatabase",
-        element: <TestDatabase />,
-      },
-      {
-        path: "user",
-        element: <Login />,
-      },
-      {
-        path: "signup",
-        element: <SignUp />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-    ],
-  },
-]);
-
 
 
 
@@ -60,7 +16,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router} />
+      <App/>
     <ReactQueryDevtools position="bottom-right" />
     </QueryClientProvider>
   </React.StrictMode>,
